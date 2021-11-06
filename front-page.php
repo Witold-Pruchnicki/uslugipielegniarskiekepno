3bcde430c369023862ca652902298de22115aaa1
Witold Pruchnicki <witekpruchnicki@gmail.com>
2021-11-06 16:23:02 +0100

    completed section aboutUs

--- front-page.php
+++ front-page.php
@@ -1,25 +1,52 @@
 <?php get_header(); ?>
     <main>
-        <section class="header-section">
+        <section class=" section header-section">
             <div class="container">
-                <div class="header-section-wrapper">
-                    <div class="header-section__text-wrapper">
+                <div class="section-wrapper">
+                    <div class="section__text-wrapper">
                         <h1 class="section__title header-section__title">Usługi
                             Pielęgniarskie
                             Kępno</h1>
-                        <p class="section__text header-section__text">
+                        <p class="section__text">
                             Jeśli szukasz pielęgniarki w Kępnie, która zapewni opiekę zdrowotną w domu, kiedy, gdzie i
                             jak
                             tego potrzebujesz, jesteś w dobrym miejscu.
                         </p>
-                        <a href="" class="btn">Umów wizytę</a>
+                        <a href="" class="header-section__btn">Umów wizytę</a>
                     </div>
-                    <div class="header-section__image-wrapper">
+                    <div class="section__image-wrapper">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/header-section__img.svg"
                              alt="" class="header-section__img">
                     </div>
                 </div>
             </div>
+        </section>
+        <section class=" section aboutUs-section">
+            <div class="container">
+                <div class="section-wrapper">
+                    <div class="section__image-wrapper">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/uslugipielegniarskiekepbo-o-nas.jpg"
+                             alt="" class="aboutUs-section__img">
+                    </div>
+                    <div class="section__text-wrapper">
+                        <h2 class="section__intro">O nas </h2>
+                        <h3 class="section__title">Wyspecjalizowane Pielęgniarki </h3>
+                        <p class="section__text">Ponad 25 letni staż pracy na oddziale szpitalnym 10 letnie
+                            doświadczenie w opiece domowej nad pacjentem ukończone studia pielęgniarskie, specjalizacja
+                            opieki paliatywnej oraz liczne kursy specjalistyczne.</p>
+                        <ul class="aboutUs__features-list">
+                            <li class="feature"><img class="feature__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/feature.png" alt="">Wyspecjalizowany i doświadczony personel</li>
+                            <li class="feature"><img class="feature__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/feature.png" alt="">Wizyty w dogodnym terminie</li>
+                            <li class="feature"><img class="feature__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/feature.png" alt="">Realizacja usługi w domu pacjenta </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+        <section class="section">
+
         </section>
     </main>
 
